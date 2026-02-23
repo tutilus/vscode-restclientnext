80ae2a814c482843c6ba278690f2f373eb17ae11
tutilus <au.tutilus@gmail.com>
2026-02-23 20:14:34 +0100

    Fix update dependencies (#1) (#2)
    * Clone Huachao REST Client and update dependencies

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,95 @@
+'use strict';
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { commands, ExtensionContext, languages, Range, TextDocument, Uri, window, workspace } from 'vscode';
+import { CodeSnippetController } from './controllers/codeSnippetController';
+import { EnvironmentController } from './controllers/environmentController';
+import { HistoryController } from './controllers/historyController';
+import { RequestController } from './controllers/requestController';
+import { SwaggerController } from './controllers/swaggerController';
+import { CustomVariableDiagnosticsProvider } from "./providers/customVariableDiagnosticsProvider";
+import { RequestBodyDocumentLinkProvider } from './providers/documentLinkProvider';
+import { EnvironmentOrFileVariableHoverProvider } from './providers/environmentOrFileVariableHoverProvider';
+import { FileVariableDefinitionProvider } from './providers/fileVariableDefinitionProvider';
+import { FileVariableReferenceProvider } from './providers/fileVariableReferenceProvider';
+import { FileVariableReferencesCodeLensProvider } from './providers/fileVariableReferencesCodeLensProvider';
+import { HttpCodeLensProvider } from './providers/httpCodeLensProvider';
+import { HttpCompletionItemProvider } from './providers/httpCompletionItemProvider';
+import { HttpDocumentSymbolProvider } from './providers/httpDocumentSymbolProvider';
+import { MarkdownCodeLensProvider } from './providers/markdownCodeLensProvider';
+import { RequestVariableCompletionItemProvider } from "./providers/requestVariableCompletionItemProvider";
+import { RequestVariableDefinitionProvider } from './providers/requestVariableDefinitionProvider';
+import { RequestVariableHoverProvider } from './providers/requestVariableHoverProvider';
+import { AadTokenCache } from './utils/aadTokenCache';
+import { ConfigurationDependentRegistration } from './utils/dependentRegistration';
+import { UserDataManager } from './utils/userDataManager';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export async function activate(context: ExtensionContext) {
+    await UserDataManager.initialize();
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "rest-client-next" is now active!');
+    const requestController = new RequestController(context);
+    const historyController = new HistoryController();
+    const codeSnippetController = new CodeSnippetController(context);
+    const environmentController = await EnvironmentController.create();
+    const swaggerController = new SwaggerController(context);
+    context.subscriptions.push(requestController);
+    context.subscriptions.push(historyController);
+    context.subscriptions.push(codeSnippetController);
+    context.subscriptions.push(environmentController);
+    context.subscriptions.push(commands.registerCommand('rest-client.request', ((_document: TextDocument, range: Range) => requestController.run(range))));
+    context.subscriptions.push(commands.registerCommand('rest-client.rerun-last-request', () => requestController.rerun()));
+    context.subscriptions.push(commands.registerCommand('rest-client.cancel-request', () => requestController.cancel()));
+    context.subscriptions.push(commands.registerCommand('rest-client.history', () => historyController.save()));
+    context.subscriptions.push(commands.registerCommand('rest-client.clear-history', () => historyController.clear()));
+    context.subscriptions.push(commands.registerCommand('rest-client.generate-codesnippet', () => codeSnippetController.run()));
+    context.subscriptions.push(commands.registerCommand('rest-client.copy-request-as-curl', () => codeSnippetController.copyAsCurl()));
+    context.subscriptions.push(commands.registerCommand('rest-client.switch-environment', () => environmentController.switchEnvironment()));
+    context.subscriptions.push(commands.registerCommand('rest-client.clear-aad-token-cache', () => AadTokenCache.clear()));
+    context.subscriptions.push(commands.registerCommand('rest-client.clear-cookies', () => requestController.clearCookies()));
+    context.subscriptions.push(commands.registerCommand('rest-client._openDocumentLink', args => {
+        workspace.openTextDocument(Uri.parse(args.path)).then(window.showTextDocument, error => {
+            window.showErrorMessage(error.message);
+        });
+    }));
+    context.subscriptions.push(commands.registerCommand('rest-client.import-swagger', async () => swaggerController.import()));
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('rest-client-next.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from REST Client Next, i am here!');
-	});
 
-	context.subscriptions.push(disposable);
+    const documentSelector = [
+        { language: 'http', scheme: '*' }
+    ];
+
+    const mdDocumentSelector = [
+        { language: 'markdown', scheme: '*' }
+    ];
+
+    context.subscriptions.push(languages.registerCompletionItemProvider(documentSelector, new HttpCompletionItemProvider()));
+    context.subscriptions.push(languages.registerCompletionItemProvider(documentSelector, new RequestVariableCompletionItemProvider(), '.'));
+    context.subscriptions.push(languages.registerHoverProvider(documentSelector, new EnvironmentOrFileVariableHoverProvider()));
+    context.subscriptions.push(languages.registerHoverProvider(documentSelector, new RequestVariableHoverProvider()));
+    context.subscriptions.push(
+        new ConfigurationDependentRegistration(
+            () => languages.registerCodeLensProvider(documentSelector, new HttpCodeLensProvider()),
+            s => s.enableSendRequestCodeLens));
+    context.subscriptions.push(
+        new ConfigurationDependentRegistration(
+            () => languages.registerCodeLensProvider(documentSelector, new FileVariableReferencesCodeLensProvider()),
+            s => s.enableCustomVariableReferencesCodeLens));
+    context.subscriptions.push(
+        new ConfigurationDependentRegistration(
+            () => languages.registerCodeLensProvider(mdDocumentSelector, new MarkdownCodeLensProvider()),
+            s => s.enableSendRequestCodeLens));
+    context.subscriptions.push(languages.registerDocumentLinkProvider(documentSelector, new RequestBodyDocumentLinkProvider()));
+    context.subscriptions.push(languages.registerDefinitionProvider(documentSelector, new FileVariableDefinitionProvider()));
+    context.subscriptions.push(languages.registerDefinitionProvider(documentSelector, new RequestVariableDefinitionProvider()));
+    context.subscriptions.push(languages.registerReferenceProvider(documentSelector, new FileVariableReferenceProvider()));
+    context.subscriptions.push(languages.registerDocumentSymbolProvider(documentSelector, new HttpDocumentSymbolProvider()));
+
+    const diagnosticsProvider = new CustomVariableDiagnosticsProvider();
+    context.subscriptions.push(diagnosticsProvider);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+// this method is called when your extension is deactivated
+export function deactivate() {
+}
---
+++ src/utils/httpClient.ts
@@ -0,0 +1,317 @@
+import * as fs from 'fs-extra';
+import * as iconv from 'iconv-lite';
+import * as path from 'path';
+import { CookieJar } from 'tough-cookie';
+import FileCookieStore from 'tough-cookie-file-store';
+import * as url from 'url';
+import { Uri, window } from 'vscode';
+import { RequestHeaders, ResponseHeaders } from '../models/base';
+import { IRestClientSettings, SystemSettings } from '../models/configurationSettings';
+import { HttpRequest } from '../models/httpRequest';
+import { HttpResponse } from '../models/httpResponse';
+import { awsCognito } from './auth/awsCognito';
+import { awsSignature } from './auth/awsSignature';
+import { digest } from './auth/digest';
+import { MimeUtility } from './mimeUtility';
+import { getHeader, removeHeader } from './misc';
+import { convertBufferToStream, convertStreamToBuffer } from './streamUtility';
+import { UserDataManager } from './userDataManager';
+import { getCurrentHttpFileName, getWorkspaceRootPath } from './workspaceUtility';
+
+import got from 'got';
+import { CancelableRequest, Headers, Method, OptionsOfBufferResponseBody, Response } from 'got';
+
+const encodeUrl = require('encodeurl');
+
+type Certificate = {
+    cert?: Buffer;
+    key?: Buffer;
+    pfx?: Buffer;
+    passphrase?: string;
+};
+
+export class HttpClient {
+    private cookieStore: CookieJar;
+
+    public constructor() {
+        const cookieFilePath = UserDataManager.cookieFilePath;
+        this.cookieStore = new CookieJar(new FileCookieStore(cookieFilePath));
+    }
+
+    public async send(httpRequest: HttpRequest, settings?: IRestClientSettings): Promise<HttpResponse> {
+        settings = settings || SystemSettings.Instance;
+
+        const options = await this.prepareOptions(httpRequest, settings);
+
+        let bodySize = 0;
+        let headersSize = 0;
+        const requestUrl = encodeUrl(httpRequest.url);
+        const request: CancelableRequest<Response<Buffer>> = got(requestUrl, options);
+        httpRequest.setUnderlyingRequest(request);
+        (request as any).on('response', (res: { rawHeaders: any[]; on: (arg0: string, arg1: (chunk: any) => void) => void; }) => {
+            if (res.rawHeaders) {
+                headersSize += res.rawHeaders.map(h => h.length).reduce((a, b) => a + b, 0);
+                headersSize += (res.rawHeaders.length) / 2;
+            }
+            res.on('data', chunk => {
+                bodySize += chunk.length;
+            });
+        });
+
+        const response = await request;
+
+        const contentType = response.headers['content-type'];
+        let encoding: string | undefined;
+        if (contentType) {
+            encoding = MimeUtility.parse(contentType).charset;
+        }
+
+        if (!encoding) {
+            encoding = "utf8";
+        }
+
+        const bodyBuffer = response.body;
+        let bodyString = iconv.encodingExists(encoding) ? iconv.decode(bodyBuffer, encoding) : bodyBuffer.toString();
+
+        if (settings.decodeEscapedUnicodeCharacters) {
+            bodyString = this.decodeEscapedUnicodeCharacters(bodyString);
+        }
+
+        // adjust response header case, due to the response headers in nodejs http module is in lowercase
+        const responseHeaders: ResponseHeaders = HttpClient.normalizeHeaderNames(response.headers, response.rawHeaders);
+
+        const requestBody = options.body;
+
+        return new HttpResponse(
+            response.statusCode,
+            response.statusMessage!,
+            response.httpVersion,
+            responseHeaders,
+            bodyString,
+            bodySize,
+            headersSize,
+            bodyBuffer,
+            response.timings.phases,
+            new HttpRequest(
+                options.method!,
+                requestUrl,
+                HttpClient.normalizeHeaderNames(
+                    (response as any).request.options.headers as RequestHeaders,
+                    Object.keys(httpRequest.headers)),
+                Buffer.isBuffer(requestBody)
+                    ? convertBufferToStream(requestBody)
+                    : (typeof requestBody === 'string' || requestBody === undefined
+                        ? requestBody
+                        : undefined),
+                httpRequest.rawBody,
+                httpRequest.name
+            ));
+    }
+
+    public async clearCookies() {
+        await fs.remove(UserDataManager.cookieFilePath);
+        this.cookieStore = new CookieJar(new FileCookieStore(UserDataManager.cookieFilePath));
+    }
+
+    private async prepareOptions(httpRequest: HttpRequest, settings: IRestClientSettings): Promise<OptionsOfBufferResponseBody> {
+        const originalRequestBody = httpRequest.body;
+        let requestBody: string | Buffer | undefined;
+        if (originalRequestBody) {
+            if (typeof originalRequestBody !== 'string') {
+                requestBody = await convertStreamToBuffer(originalRequestBody);
+            } else {
+                requestBody = originalRequestBody;
+            }
+        }
+
+        // Fix #682 Do not touch original headers in httpRequest, which may be used for retry later
+        // Simply do a shadow copy here
+        const clonedHeaders = Object.assign({}, httpRequest.headers);
+
+        const options: OptionsOfBufferResponseBody = {
+            headers: clonedHeaders as any as Headers,
+            method: httpRequest.method as any as Method,
+            body: requestBody,
+            responseType: 'buffer',
+            decompress: true,
+            followRedirect: settings.followRedirect,
+            throwHttpErrors: false,
+            retry: { limit: 0 },
+            hooks: {
+                afterResponse: [],
+                beforeRequest: [],
+            },
+            https: {
+                rejectUnauthorized: false
+            }
+        };
+
+        if (settings.timeoutInMilliseconds > 0) {
+            options.timeout = { request: settings.timeoutInMilliseconds };
+        }
+
+        if (settings.rememberCookiesForSubsequentRequests) {
+            options.cookieJar = this.cookieStore;
+        }
+
+        // TODO: refactor auth
+        const authorization = getHeader(options.headers!, 'Authorization') as string | undefined;
+        if (authorization) {
+            const [scheme, user, ...args] = authorization.split(/\s+/);
+            const normalizedScheme = scheme.toLowerCase();
+            if (args.length > 0) {
+                const pass = args.join(' ');
+                if (normalizedScheme === 'basic') {
+                    removeHeader(options.headers!, 'Authorization');
+                    options.username = user;
+                    options.password = pass;
+                } else if (normalizedScheme === 'digest') {
+                    removeHeader(options.headers!, 'Authorization');
+                    options.hooks!.afterResponse!.push(digest(user, pass));
+                } else if (normalizedScheme === 'aws') {
+                    removeHeader(options.headers!, 'Authorization');
+                    options.hooks!.beforeRequest!.push(awsSignature(authorization));
+                } else if (normalizedScheme === 'cognito') {
+                    removeHeader(options.headers!, 'Authorization');
+                   options.hooks!.beforeRequest!.push(await awsCognito(authorization));
+                }
+            } else if (normalizedScheme === 'basic' && user.includes(':')) {
+                removeHeader(options.headers!, 'Authorization');
+                const [username, password] = user.split(':');
+                options.username = username;
+                options.password = password;
+            }
+        }
+
+        // set certificate
+        const certificate = this.getRequestCertificate(httpRequest.url, settings);
+        Object.assign(options, certificate);
+
+        // set proxy
+        if (
+            settings.proxy &&
+            !HttpClient.ignoreProxy(httpRequest.url, settings.excludeHostsForProxy)
+        ) {
+            if (httpRequest.url.startsWith('http:')) {
+                const { HttpProxyAgent } = await import('http-proxy-agent');
+                options.agent = {
+                    http: new HttpProxyAgent(settings.proxy)
+                };
+            } else {
+                const { HttpsProxyAgent } = await import('https-proxy-agent');
+                options.agent = {
+                    https: new HttpsProxyAgent(settings.proxy)
+                };
+            }
+        }
+
+        return options;
+    }
+
+    private decodeEscapedUnicodeCharacters(body: string): string {
+        return body.replace(/\\u([0-9a-fA-F]{4})/gi, (_, g) => {
+            const char = String.fromCharCode(parseInt(g, 16));
+            return char === '"' ? '\\"' : char;
+        });
+    }
+
+    private getRequestCertificate(requestUrl: string, settings: IRestClientSettings): Certificate | null {
+        const host = url.parse(requestUrl).host;
+        if (!host || !(host in settings.hostCertificates)) {
+            return null;
+        }
+
+        const { cert: certPath, key: keyPath, pfx: pfxPath, passphrase } = settings.hostCertificates[host];
+        const cert = this.resolveCertificate(certPath);
+        const key = this.resolveCertificate(keyPath);
+        const pfx = this.resolveCertificate(pfxPath);
+        return { cert, key, pfx, passphrase };
+    }
+
+    private static ignoreProxy(requestUrl: string, excludeHostsForProxy: string[]): Boolean {
+        if (!excludeHostsForProxy || excludeHostsForProxy.length === 0) {
+            return false;
+        }
+
+        const resolvedUrl = url.parse(requestUrl);
+        const hostName = resolvedUrl.hostname?.toLowerCase();
+        const port = resolvedUrl.port;
+        const excludeHostsProxyList = Array.from(new Set(excludeHostsForProxy.map(eh => eh.toLowerCase())));
+
+        for (const eh of excludeHostsProxyList) {
+            const urlParts = eh.split(":");
+            if (!port) {
+                // if no port specified in request url, host name must exactly match
+                if (urlParts.length === 1 && urlParts[0] === hostName) {
+                    return true;
+                }
+            } else {
+                // if port specified, match host without port or hostname:port exactly match
+                const [ph, pp] = urlParts;
+                if (ph === hostName && (!pp || pp === port)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private resolveCertificate(absoluteOrRelativePath: string | undefined): Buffer | undefined {
+        if (absoluteOrRelativePath === undefined) {
+            return undefined;
+        }
+
+        if (path.isAbsolute(absoluteOrRelativePath)) {
+            if (!fs.existsSync(absoluteOrRelativePath)) {
+                window.showWarningMessage(`Certificate path ${absoluteOrRelativePath} doesn't exist, please make sure it exists.`);
+                return undefined;
+            } else {
+                return fs.readFileSync(absoluteOrRelativePath);
+            }
+        }
+
+        // the path should be relative path
+        const rootPath = getWorkspaceRootPath();
+        let absolutePath = '';
+        if (rootPath) {
+            absolutePath = path.join(Uri.parse(rootPath).fsPath, absoluteOrRelativePath);
+            if (fs.existsSync(absolutePath)) {
+                return fs.readFileSync(absolutePath);
+            } else {
+                window.showWarningMessage(`Certificate path ${absoluteOrRelativePath} doesn't exist, please make sure it exists.`);
+                return undefined;
+            }
+        }
+
+        const currentFilePath = getCurrentHttpFileName();
+        if (!currentFilePath) {
+            return undefined;
+        }
+
+        absolutePath = path.join(path.dirname(currentFilePath), absoluteOrRelativePath);
+        if (fs.existsSync(absolutePath)) {
+            return fs.readFileSync(absolutePath);
+        } else {
+            window.showWarningMessage(`Certificate path ${absoluteOrRelativePath} doesn't exist, please make sure it exists.`);
+            return undefined;
+        }
+    }
+
+    private static normalizeHeaderNames<T extends RequestHeaders | ResponseHeaders>(headers: T, rawHeaders: string[]): T {
+        const headersDic: { [key: string]: string } = rawHeaders.reduce(
+            (prev: { [key: string]: string }, cur: string) => {
+                if (!(cur.toLowerCase() in prev)) {
+                    prev[cur.toLowerCase()] = cur;
+                }
+                return prev;
+            }, {});
+        const adjustedResponseHeaders = {} as RequestHeaders | ResponseHeaders;
+        for (const header in headers) {
+            const adjustedHeaderName = headersDic[header] || header;
+            adjustedResponseHeaders[adjustedHeaderName] = headers[header];
+        }
+
+        return adjustedResponseHeaders as T;
+    }
+}
