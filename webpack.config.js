c27fa20c0e2d80564c4349ff1900075f6250029e
Tutilus <au.tutilus@gmail.com>
2026-07-19 13:02:33 +0200

    chore: Add kerberos into external webpack properties to delete test errors

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ const extensionConfig = {
     },
     externals: {
         vscode: 'commonjs vscode', // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+        kerberos: 'commonjs kerberos',
         // modules added here also need to be added in the .vscodeignore file
     },
     resolve: {
